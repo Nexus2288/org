41fbb9baf2afcf45a6dcf444809187a79bda3255
Nexus2288 <osvpriyanshu09@gmail.com>
2025-05-06 12:16:27 +0530

    Delete script1.js

--- script1.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const buttons = document.querySelectorAll(".btn");
-    buttons.forEach((btn, index) => {
-        setTimeout(() => {
-            btn.style.opacity = "1";
-            btn.style.transform = "translateY(0)";
-        }, 500 + index * 300);
-    });
-});
